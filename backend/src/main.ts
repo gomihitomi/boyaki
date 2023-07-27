e18ac07f16e17e192ec6a6d8c9589d6fbab319c0
gomihitomi <gomihitomi@gmail.com>
2023-07-27 14:57:45 +0000

    だるくなった2

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { AppModule } from '@app/app.module';
-import { NestFactory } from '@nestjs/core';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableShutdownHooks();
-  await app.listen(3001);
-}
-bootstrap();
--- frontend/src/app/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <small>© 吾味人美</small>
-    </footer>
-  );
-}
