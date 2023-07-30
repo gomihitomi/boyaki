3dc1c22ab44114cbc0712ba24652f4fd40f67f05
gomihitomi <gomihitomi@gmail.com>
2023-07-30 06:35:42 +0000

    ぬ

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+// GithubPagesに公開する時、絶対パス指定だと静的ファイルが読み込めないのでレポジトリ名を設定
+// TODO: ローカルでビルドしても/boyakiのprefix付くから直しといて
+const prefixPath = process.env.NODE_ENV === "production" ? "/boyaki" : "";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  assetPrefix: prefixPath,
+  basePath: prefixPath,
 };
 
 module.exports = nextConfig;
