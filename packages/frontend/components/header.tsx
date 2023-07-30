b4c30bd7a3f69575a70a95e21e163b1ecc651265
gomihitomi <gomihitomi@gmail.com>
2023-07-30 05:53:18 +0000

    test

--- packages/frontend/components/header.tsx
+++ packages/frontend/components/header.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default async function Header() {
   return (
     <header className="py-8">
-      <Link href="./">
+      <Link href="/">
         <h1 className="text-3xl font-bold">吾味人美のぼやき</h1>
       </Link>
-      <Link href="./link" className="text-emerald-700 font-bold">
+      <Link href="/link" className="text-emerald-700 font-bold">
         リンク集
       </Link>
     </header>
--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
+  assetPrefix: process.env.BASE_PATH || "",
+  basePath: process.env.BASE_PATH || "",
 };
 
 module.exports = nextConfig;
