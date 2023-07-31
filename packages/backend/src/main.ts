a5e91145dd6ef4192139ab34df4995eb48da02eb
gomihitomi <gomihitomi@gmail.com>
2023-07-31 14:50:30 +0000

    ポート周りとかを整理した

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,12 +1,8 @@
 import { AppModule } from '@/app/app.module';
 import { NestFactory } from '@nestjs/core';
 
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.BACKEND_PORT ?? '3001';
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
