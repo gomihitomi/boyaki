b46ffe062a61e67389a69b5314e6a8dc83e9a910
gomihitomi <gomihitomi@gmail.com>
2023-07-31 12:24:21 +0000

    細かい所を修正

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-// GithubPagesに公開する時、絶対パス指定だと静的ファイルが読み込めないのでレポジトリ名を設定
-// TODO: ローカルでビルドしても/boyakiのprefix付くから直しといて
-const prefixPath = process.env.NODE_ENV === "production" ? "/boyaki" : "";
+const prefixPath = process.env.PREFIX_PATH;
 
 const nextConfig = {
   output: "export",
