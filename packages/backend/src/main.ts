023ec5d365e60b958e4f495b92c296471a81d4f9
gomihitomi <gomihitomi@gmail.com>
2023-07-31 14:32:43 +0000

    そもそもdotenvの使ってなかった

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,6 +1,9 @@
 import { AppModule } from '@/app/app.module';
 import { NestFactory } from '@nestjs/core';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const port = process.env.BACKEND_PORT ?? '3001';
 
 async function bootstrap() {
