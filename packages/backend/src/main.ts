560f52d80940b6ad3fbf799a7dc9d5130ee588f3
gomihitomi <gomihitomi@gmail.com>
2023-07-31 14:42:04 +0000

    はぁ3

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,15 +1,12 @@
 import { AppModule } from '@/app/app.module';
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.BACKEND_PORT ?? '3001';
-
 async function bootstrap() {
-  Logger.warn(`PORT is ${port}`);
   const app = await NestFactory.create(AppModule);
+  const port = process.env.BACKEND_PORT ?? '3001';
   await app.listen(port);
 }
 bootstrap();
