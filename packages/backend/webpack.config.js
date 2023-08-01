025b81722df97eba7afaf6ed2fabaad028b6c4f8
gomihitomi <gomihitomi@gmail.com>
2023-08-01 03:20:49 +0000

    webpack追加

---
+++ packages/backend/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  target: 'node',
+  mode: process.env.NODE_ENV ?? 'production',
+  externals: [
+    {
+      '@nestjs/websockets/socket-module':
+        'commonjs2 @nestjs/websockets/socket-module',
+      '@nestjs/microservices/microservices-module':
+        'commonjs2 @nestjs/microservices/microservices-module',
+    },
+  ],
+  optimization: {
+    minimize: true,
+  },
+};
