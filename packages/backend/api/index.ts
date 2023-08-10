6b684d859c6cfe37ac6c4a11e91ae0fb0d0af283
gomihitomi <gomihitomi@gmail.com>
2023-08-10 12:08:12 +0000

    zzz

--- packages/backend/api/index.ts
+++ packages/backend/api/index.ts
@@ -12,10 +12,13 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", `${process.env.CORS}`);
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTION"
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   next();
 });
 
