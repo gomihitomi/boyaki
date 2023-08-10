164df84df258be6adc4056e4d9a2b1352a2016da
gomihitomi <gomihitomi@gmail.com>
2023-08-10 08:54:39 +0000

    z

--- packages/backend/api/index.ts
+++ packages/backend/api/index.ts
@@ -5,7 +5,7 @@ import router from "./route";
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use("/", router);
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
