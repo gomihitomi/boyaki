fe276378d01e7929d5455c9148793869894b4be2
gomihitomi <gomihitomi@gmail.com>
2023-08-10 08:52:07 +0000

    aaa

--- packages/backend/api/route.ts
+++ packages/backend/api/route.ts
@@ -2,18 +2,14 @@ import { getPostDetail } from "@boyaki/lib";
 import * as Express from "express";
 const router = Express.Router();
 
-router.get("/", (req, res) => {
-  res.send("boyaki api.");
-});
-
-router.get("/:id", async (req, res) => {
-  const slug = req.params.id;
-  if (slug === "favicon.ico") {
+router.get("/", async (req, res) => {
+  const slug = req.query.slug;
+  if (!slug || slug === "favicon.ico") {
     return res.send("");
   }
-  const detail = await getPostDetail(req.params.id);
+  const detail = await getPostDetail(slug as string);
   if (detail.contents.length !== 1) {
-    console.warn(`slug: ${req.params.id} のコンテンツ件数が1件以外です。`);
+    console.warn(`slug: ${slug as string} のコンテンツ件数が1件以外です。`);
     res.status(500);
     res.send({ like: 0, comments: [] });
     return;
